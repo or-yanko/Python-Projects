7cdf6d423806a7556ef4849de15ac6e9f5d2ebca
or yanko <or@mytay.co.il>
2022-03-04 19:40:56 +0200

    b

--- crypto-bots/bot1.py
+++ crypto-bots/bot1.py
@@ -1,3 +1,6 @@
+'''
+---------------------show coin live prices----------------------
+'''
 import pandas as pd
 from bs4 import BeautifulSoup as BS
 import requests
@@ -26,19 +29,25 @@ def main():
     now = datetime.now()
 
     last_price = '0'
+    crypto = input('enter crypto coin to see his live price: ')
     while True:
-        crypto = 'bitcoin' 
-        price = get_crypto_price(crypto)
-        onlyprice = price.split(' ')[0]
-        if onlyprice != last_price:
-            if float(last_price.replace(',',''))<float(onlyprice.replace(',','')):
-                print(crypto+' price: ',colored(onlyprice, 'green'), 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
-            elif onlyprice < last_price:
-                print(crypto+' price: ',colored(onlyprice, 'red'), 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
-            else:
-                print(crypto+' price: ',onlyprice, 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
-            last_price = onlyprice 
-        time.sleep(10)
+        try:
+            price = get_crypto_price(crypto)
+            onlyprice = price.split(' ')[0]
+            if onlyprice != last_price:
+                now = datetime.now()
+                if float(last_price.replace(',',''))<float(onlyprice.replace(',','')):
+                    print(crypto+' price: ',colored(onlyprice, 'green'), 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
+                elif onlyprice < last_price:
+                    print(crypto+' price: ',colored(onlyprice, 'red'), 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
+                else:
+                    print(crypto+' price: ',onlyprice, 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
+                last_price = onlyprice 
+            time.sleep(10)
+        except KeyError:
+                print(colored(str(crypto+' is wrong coin name... '), 'red'))
+                break
+
 
 
 
