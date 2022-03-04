3655effb9501ac543781ee230b8e31d745376bdf
or yanko <or@mytay.co.il>
2022-03-04 22:04:16 +0200

    b

--- crypto-bots/bot1.py
+++ crypto-bots/bot1.py
@@ -1,6 +1,7 @@
 '''
 ---------------------show coin live prices----------------------
 '''
+import multiprocessing
 import pandas as pd
 from bs4 import BeautifulSoup as BS
 import requests
@@ -8,6 +9,7 @@ import time
 from datetime import datetime
 from termcolor import colored
 
+path = 'coins-logs/'
 
 def get_crypto_price(coin):
 #Get the URL
@@ -25,28 +27,56 @@ def get_crypto_price(coin):
     #Return the text 
     return text
 
-def main():
-    now = datetime.now()
-
+def check_coin(crypto):
     last_price = '0'
-    crypto = input('enter crypto coin to see his live price: ')
+
     while True:
         try:
             price = get_crypto_price(crypto)
             onlyprice = price.split(' ')[0]
             if onlyprice != last_price:
                 now = datetime.now()
+                '''
                 if float(last_price.replace(',',''))<float(onlyprice.replace(',','')):
-                    print(crypto+' price: ',colored(onlyprice, 'green'), 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
+                    print(crypto+' price:',colored(onlyprice, 'green'), 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
                 elif onlyprice < last_price:
-                    print(crypto+' price: ',colored(onlyprice, 'red'), 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
+                    print(crypto+' price:',colored(onlyprice, 'red'), 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
                 else:
-                    print(crypto+' price: ',onlyprice, 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
-                last_price = onlyprice 
+                    print(crypto+' price:',onlyprice, 'ILS at', now.strftime("%d/%m/%Y %H:%M:%S")) 
+                    '''
+                last_price = onlyprice
+                with open(str(path + crypto + "-logs.txt"), "a") as text_file:
+                    text_file.write( onlyprice + " " + now.strftime("%d/%m/%Y %H:%M:%S") + "\n")
+                    text_file.close()
+            else:
+                print(colored(str(crypto + ' hasn\'t changed yet'), 'yellow'))
             time.sleep(10)
-        except KeyError:
-                print(colored(str(crypto+' is wrong coin name... '), 'red'))
-                break
+        except :
+            print(colored(str(crypto+' is wrong coin name... '), 'red'))
+            break
+
+def main():
+
+    crypto = ' '
+    crypto = input('enter crypto coin to see his live price: ')
+    if crypto.lower()  in ['exit','quit','bye','pipi']:
+        print(colored('bye bye... :-(', 'red'))
+    check_coin(crypto)
+
+    '''
+    processes = []
+    crypto = ' '
+    while True:
+        print(processes)
+        crypto = input('enter crypto coin to see his live price: ')
+        if crypto.lower()  in ['exit','quit','bye','pipi']:
+            print(colored('bye bye... :-(', 'red'))
+            break
+        processes.append(multiprocessing.Process(target=check_coin, args=(crypto)))
+        processes[-1].start()
+        for proc in processes:
+            proc.join()
+    '''
 
 
 
