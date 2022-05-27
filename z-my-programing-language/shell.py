9af817a5fc78219a3040f57e2caf4356ad22ee76
or yanko <54112968+or-yanko@users.noreply.github.com>
2022-05-27 23:21:08 +0300

    Update shell.py

--- z-my-programing-language/shell.py
+++ z-my-programing-language/shell.py
@@ -5,9 +5,8 @@ import datetime
 
 while True:
     currentDT = datetime.datetime.now()
-    #date = f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}, pos {self.pos_start.col + 1}'
     text = input(
-        colored(f'\nOR YANKO\'s programing language      {currentDT.day}/{currentDT.month}/{currentDT.year} at {currentDT.hour}:{currentDT.minute} :\nbasic => ', 'blue'))
+        colored(f'\nOR YANKO\'s programing language {currentDT.day}/{currentDT.month}/{currentDT.year} at {currentDT.hour}:{currentDT.minute} :\nbasic => ', 'blue'))
     result, error = basic.run('<stdin>', text)
 
     if error:
