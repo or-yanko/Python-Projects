49d8786c21c52e9ac7c9b25d8cb7a3f54c42c386
or yanko <54112968+or-yanko@users.noreply.github.com>
2022-05-27 23:15:37 +0300

    my programming language 👨‍💻

--- z-my-programing-language/shell.py
+++ z-my-programing-language/shell.py
@@ -1,12 +1,16 @@
 import basic
-
-def analize_line():
-    text = input('basic > ')
-    result, error = basic.run('<stdin>', text)
-
-    if error: print(error.as_string())
-    else: print(result)
+from termcolor import colored
+import datetime
 
 
 while True:
-    analize_line()
+    currentDT = datetime.datetime.now()
+    #date = f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}, pos {self.pos_start.col + 1}'
+    text = input(
+        colored(f'\nOR YANKO\'s programing language      {currentDT.day}/{currentDT.month}/{currentDT.year} at {currentDT.hour}:{currentDT.minute} :\nbasic => ', 'blue'))
+    result, error = basic.run('<stdin>', text)
+
+    if error:
+        print(colored(error.as_string(), 'red'))
+    else:
+        print(colored(result, 'green'))
