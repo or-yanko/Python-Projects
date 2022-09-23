34ec6ec47b2cbe3e1b136efda227bd21cb434d4d
or yanko <54112968+or-yanko@users.noreply.github.com>
2022-09-23 22:54:42 +0300

    .

--- django projects/firstDjangoProject/README.txt
+++
@@ -1,1 +0,0 @@
-users tasks web' you write task and make it V or X and can insert the tasks
--- django projects/firstDjangoProject/myAppName/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TodoList, Item
-
-# Register your models here.
-admin.site.register(TodoList)
-admin.site.register(Item)
-
