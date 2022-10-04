b5e62a9bf7adb5e7bb1914818e051000ca32af30
or yanko <54112968+or-yanko@users.noreply.github.com>
2022-10-04 18:56:53 +0300

    Create square-spiral.py

---
+++ turtle/square-spiral.py
@@ -0,0 +1,9 @@
+import turtle
+
+squary = turtle.Turtle()
+squary.speed(10)
+
+for i in range(500):  # this "for" loop will repeat these functions 500 times
+    squary.forward(i)
+    squary.left(91)
+turtle.done()
